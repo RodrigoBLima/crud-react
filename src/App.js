8f67bb353916c951abded7b343c7aeeab4293690
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-05-14 16:39:31 -0300

    crud o.k and add others functions and layout

--- src/App.js
+++ src/App.js
@@ -18,20 +18,25 @@ class App extends Component{
   fSubmit = (e) =>{
     e.preventDefault();
     console.log("ok");
-    
+
     let datas = this.state.datas;
     let name = this.refs.name.value;
     let address = this.refs.address.value;
+    let email = this.refs.email.value;
+    let cep = this.refs.cep.value;
 
     if(this.state.act === 0){//new
       let data = {
-        name, address
+        name, address, email, cep
       }
       datas.push(data);
     }else{//update
       let index = this.state.index;
       datas[index].name = name;
       datas[index].address = address;
+      datas[index].email = email;
+      datas[index].cep = cep;
+
     }
 
     let data = {
@@ -68,6 +73,8 @@ class App extends Component{
   let datas = this.state.datas[i];
   this.refs.name.value = datas.name;
   this.refs.address.value = datas.address;
+  this.refs.email.value = datas.email;
+  this.refs.cep.value = datas.cep;
 
   this.setState({
     act:1,
@@ -81,21 +88,21 @@ class App extends Component{
     let datas = this.state.datas;
     return (
       <div className="App">
-        <h2>{this.state.title}</h2>
-
+        <h2 class="title">{this.state.title}</h2>
         <form ref="myForm" className="myForm">
-          <input type="text" ref="name" placeholder="Digite seu nome" className="formField" />
-          <input type="text" ref="address" placeholder="Digite seu endereço" className="formField" />
+          <input type="text" ref="name"  placeholder="Digite seu nome" className="formField" required />
+          <input type="text" ref="address" placeholder="Digite seu endereço" className="formField" required />
+          <input type="email" ref="email" placeholder="Digite seu e-mail" className="formField" required />
+          <input type="number" ref="cep" placeholder="Digite seu CEP" className="formField" required />
 
-          <button onClick="{(e)=>this.fSubmit(e)}" class="myButton" >Cadastrar</button>
-
+          <button onClick={(e)=>this.fSubmit(e)} className="myButton" >Cadastrar</button>
         </form>
         <pre>
          {datas.map((data, i)=>
-            <li key={1} className="myList">
-              {i+1}. {data.name}, {data.address}
-              <button onClick="{()=>this.fRemove(i)}" class="myListButton" >Excluir</button>
-              <button onClick="{()=>this.fEdit(i)}" class="myListButton" >Editar</button>
+            <li key={i} className="myList">
+              {i+1}. {data.name}, {data.address}, {data.email}, {data.cep}
+              <button onClick={()=>this.fRemove(i)} className="myListButton" >Excluir</button>
+              <button onClick={()=>this.fEdit(i)} className="myListButton" >Editar</button>
 
             </li>
           )}
