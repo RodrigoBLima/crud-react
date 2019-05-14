4e5e93e7005b061334df141043e94969a3507457
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-05-14 16:58:17 -0300

    validation

--- src/App.js
+++ src/App.js
@@ -14,6 +14,61 @@ class App extends Component{
       datas: []
     }
   }
+  handleValidation(){
+         let fields = this.state.fields;
+         let errors = {};
+         let formIsValid = true;
+
+         //Name
+         if(!fields["name"]){
+            formIsValid = false;
+            errors["name"] = "Cannot be empty";
+         }
+
+         if(typeof fields["name"] !== "undefined"){
+            if(!fields["name"].match(/^[a-zA-Z]+$/)){
+               formIsValid = false;
+               errors["name"] = "Only letters";
+            }
+         }
+
+         //Email
+         if(!fields["email"]){
+            formIsValid = false;
+            errors["email"] = "Cannot be empty";
+         }
+
+         if(typeof fields["email"] !== "undefined"){
+            let lastAtPos = fields["email"].lastIndexOf('@');
+            let lastDotPos = fields["email"].lastIndexOf('.');
+
+            if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+               formIsValid = false;
+               errors["email"] = "Email is not valid";
+             }
+        }
+
+        this.setState({errors: errors});
+        return formIsValid;
+    }
+
+    fSubmit(e){
+         e.preventDefault();
+
+         if(this.handleValidation()){
+            alert("Form submitted");
+         }else{
+            alert("Form has errors.")
+         }
+
+     }
+
+     handleChange(field, e){
+         let fields = this.state.fields;
+         fields[field] = e.target.value;
+         this.setState({fields});
+     }
+
 
   componentDidMount(){this.refs.name.focus();}
 
@@ -91,7 +146,7 @@ class App extends Component{
     return (
       <div className="App">
         <h2 class="title">{this.state.title}</h2>
-        <form ref="myForm" className="myForm">
+        <form ref="myForm" className="myForm" onSubmit= {this.fSubmit.bind(this)}>
           <input type="text" ref="name"  placeholder="Digite seu nome" className="formField" required />
           <input type="text" ref="address" placeholder="Digite seu endereço" className="formField" required />
           <input type="email" ref="email" placeholder="Digite seu e-mail" className="formField" required />
